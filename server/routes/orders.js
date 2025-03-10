e43e25d6b66020e9f4f871003edcc134e56012b8
Heisenberg794 <scgsoorya794@gmail.com>
2025-03-10 13:22:59 +0530

    11th

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../middleware/Auth');
 const Order = require('../models/Order');
 
 router.get('/', authMiddleware, async (req, res) => {
