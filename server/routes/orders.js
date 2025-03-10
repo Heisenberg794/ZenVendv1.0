a7dbbf459a6856ac9a6e428f3e9489877bda2ff6
Heisenberg794 <scgsoorya794@gmail.com>
2025-03-10 13:14:10 +0530

    8th

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('../middleware/Auth');
+const authMiddleware = require('./middleware/auth');
 const Order = require('../models/Order');
 
 router.get('/', authMiddleware, async (req, res) => {
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authMiddleware = require('./middleware/Auth');
+const authMiddleware = require('../middleware/auth');
 const Inventory = require('./models/Inventory');
 const { Server } = require('socket.io');
 const cors = require('cors');
