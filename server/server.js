a4afaed52f37050831585ff76f895f28429fd346
Heisenberg794 <scgsoorya794@gmail.com>
2025-03-10 14:53:24 +0530

    19th

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ const io = new Server(server, {
     cors: {
         origin: ['http://localhost:3000', 'https://zen-vend.vercel.app'], // Replace with Vercel URL after deployment
         methods: ['GET', 'POST', 'PUT'],
+        credentials: true
     },
 });
 
